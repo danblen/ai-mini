760f5751f232656b0f3d8fdd8483bb307944d511
ron19y <gl060698@gmail.com>
2024-03-13 12:48:01 +0800

    二维码生成

--- src/pages/QrCodeGenerator/index.jsx
+++ src/pages/QrCodeGenerator/index.jsx
@@ -18,10 +18,10 @@ export default () => {
         shareUserId: global.userInfo.data.userId,
       }); // 获取微信接口调用凭证
       // 处理响应数据，例如显示小程序码
-      const qrCodeData = Taro.arrayBufferToBase64(res.data.data);
-      const qrCodeUrl = `data:image/png;base64,${qrCodeData}`;
-      console.log('生成的小程序码链接：', qrCodeUrl);
-      setQrCodeUrl(qrCodeUrl);
+      // const qrCodeData = Taro.arrayBufferToBase64(res.data.data);
+      // const qrCodeUrl = `data:image/png;base64,${qrCodeData}`;
+      // console.log('生成的小程序码链接：', qrCodeUrl);
+      setQrCodeUrl(res.data.imageUrl);
 
       // 这里可以使用 qrCodeUrl 来展示小程序码或进行其他操作
     } catch (error) {
@@ -55,6 +55,42 @@ export default () => {
         src={qrCodeUrl}
         style="width: 200px; height: 200px; margin:100px"
       />
+      {qrCodeUrl && (
+        <View
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <View
+            style={{
+              width: '90%',
+              textAlign: 'center',
+              backgroundColor: '#fff',
+              padding: '10px 0',
+              boxShadow: '0 0 10px 0 #000000',
+              borderRadius: '5px',
+            }}
+            onClick={() => {
+              // 点击分享二维码时，调用 Taro API 分享图片
+
+              Taro.showShareMenu({
+                withShareTicket: true,
+              });
+              // Taro.downloadFile({
+              //   url: 'https://res.wx.qq.com/wxdoc/dist/assets/img/demo.ef5c5bef.jpg',
+              //   success: (res) => {
+              //     Taro.showShareImageMenu({
+              //       path: res.tempFilePath
+              //     })
+              //   }
+              // })
+            }}
+          >
+            分享二维码
+          </View>
+        </View>
+      )}
     </View>
   );
 };
