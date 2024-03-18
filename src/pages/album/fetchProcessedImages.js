84a877c26cfa51b0dbc08484d2f281dd2201f58d
huangxuezhi01 <huangxuezhi01@baidu.com>
2024-03-18 20:22:14 +0800

    修复作品无法显示

--- src/pages/album/fetchProcessedImages.js
+++ src/pages/album/fetchProcessedImages.js
@@ -7,15 +7,22 @@ export async function fetchProcessedImages(userInfo) {
         let processedImages = [];
         for (let i = 0; i < downLoadUserImages.data.length; i++) {
           let entry = downLoadUserImages.data[i];
-          let pathParts = entry['outputImagePath'].split(
-            '/home/ubuntu/code/server/'
-          );
-          let fileName = pathParts[pathParts.length - 1];
-          let imageInfo = {
-            url: 'https://facei.top/' + fileName,
-            requestId: entry['requestId'],
-          };
-          processedImages.push(imageInfo);
+          if (
+            entry['outputImagePath'] &&
+            entry['outputImagePath'].includes('/home/ubuntu/code/server/')
+          ) {
+            // 进行处理
+            let pathParts = entry['outputImagePath'].split(
+              '/home/ubuntu/code/server/'
+            );
+
+            let fileName = pathParts[pathParts.length - 1];
+            let imageInfo = {
+              url: 'https://facei.top/' + fileName,
+              requestId: entry['requestId'],
+            };
+            processedImages.push(imageInfo);
+          }
         }
         return processedImages;
       } else {
